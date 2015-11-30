cf0dc19c89374b32fbd1653cf9a34e987dd3288b
Tim Fischbach <tfischbach@codevise.de>
2015-11-30 15:49:33 +0100

    Add static avaiable_locales method

--- lib/pageflow-public-i18n.rb
+++ lib/pageflow-public-i18n.rb
@@ -1,2 +1,10 @@
 require 'pageflow/public_i18n/engine'
 require 'pageflow/public_i18n/version'
+
+module Pageflow
+  module PublicI18n
+    def self.available_locales
+      [:en, :de, :fr, :'nl-BE']
+    end
+  end
+end
