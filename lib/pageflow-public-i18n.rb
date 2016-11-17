1ce59926dec15aac54f3397b97788020dd852da6
Tim Fischbach <tfischbach@codevise.de>
2016-11-17 13:14:53 +0100

    Order locales alphabetically

--- lib/pageflow-public-i18n.rb
+++ lib/pageflow-public-i18n.rb
@@ -4,7 +4,7 @@ require 'pageflow/public_i18n/version'
 module Pageflow
   module PublicI18n
     def self.available_locales
-      [:en, :de, :fr, :es, :nl, :'nl-BE', :'cs-CZ', :'pt-BR', :sr, :tr, :gn]
+      [:'cs-CZ', :de, :en, :es, :fr, :gn, :nl, :'nl-BE', :'pt-BR', :sr, :tr]
     end
   end
 end
