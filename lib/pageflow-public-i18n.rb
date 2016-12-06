4c3bb4ad01daea85870880b053fd027d25c53954
Tim Fischbach <tfischbach@codevise.de>
2016-12-06 16:01:56 +0100

    Add el and rm locales

--- lib/pageflow-public-i18n.rb
+++ lib/pageflow-public-i18n.rb
@@ -4,7 +4,7 @@ require 'pageflow/public_i18n/version'
 module Pageflow
   module PublicI18n
     def self.available_locales
-      [:'cs-CZ', :de, :en, :es, :fr, :gn, :nl, :'nl-BE', :'pt-BR', :sr, :tr]
+      [:'cs-CZ', :de, :el, :en, :es, :fr, :gn, :nl, :'nl-BE', :'pt-BR', :rm, :sr, :tr]
     end
   end
 end
